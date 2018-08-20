8ef3872dc53542fa7383b53c6a34b90604be8d36
Victor Le Pen <victor@yarilabs.com>
2018-08-20 12:21:55 +0100

    turn everything into jekyll

--- _js.php
+++
@@ -1,5 +0,0 @@
-<!-- Javascript -->
-<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
-<script src="js/jquery/jquery.easing.js"></script>
-<script src="js/jquery/jquery.swiper.js"></script>
-<script src="js/app.js?v=<?php echo time(); ?>"></script>
